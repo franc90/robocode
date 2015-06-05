57442b39820b5991885b9ccfcf63832f53ee1710
franc90 <azarnowski1@gmail.com>
2015-06-05 21:22:00 +0200

    remove test classes

--- src/main/java/pl/edu/agh/robocode/bot/LearningRobot.java
+++ src/main/java/pl/edu/agh/robocode/bot/LearningRobot.java
@@ -1,54 +1,47 @@
 package pl.edu.agh.robocode.bot;
 
 import pl.edu.agh.robocode.bot.state.RobocodeState;
-import pl.edu.agh.robocode.bot.state.helper.RoboState;
 import pl.edu.agh.robocode.bot.state.helper.RobocodeStateHelper;
-import pl.edu.agh.robocode.bot.state.helper.StatePersistingHelper;
 import pl.edu.agh.robocode.learning.RobocodeLearningStrategy;
 import pl.edu.agh.robocode.motion.MotionAction;
 import pl.edu.agh.robocode.motion.StraightMotion;
 import pl.edu.agh.robocode.motion.TurnMotion;
 import pl.edu.agh.robocode.properties.EnvironmentProperties;
 import pl.edu.agh.robocode.properties.helper.EnvironmentPropertiesHelper;
-import robocode.*;
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
 
 import java.io.File;
 
 public class LearningRobot extends AdvancedRobot {
 
     private static final String STATE_FILE = "robotState";
-    
+
     private RobocodeStrategy strategy;
 
     private RobocodeStateHelper robocodeStateHelper = new RobocodeStateHelper();
 
     private EnvironmentPropertiesHelper environmentPropertiesHelper = new EnvironmentPropertiesHelper();
 
-    private StatePersistingHelper<RoboState> helper;
+//    private StatePersistingHelper<> helper;
 
     private EnvironmentProperties properties;
-
-    private RoboState roboState;
 
     @Override
     public void run() {
         File dataFile = getDataFile(STATE_FILE);
-        helper = new StatePersistingHelper<RoboState>(dataFile);
+//        helper = new StatePersistingHelper<RoboState>(dataFile);
         properties = environmentPropertiesHelper.generateProperties(robocodeStateHelper.create(this));
         strategy = new RobocodeLearningStrategy(properties);
 
         if (getRoundNum() > 0) {
-            roboState = helper.load();
+//            roboState = helper.load();
         }
 
         while (true) {
-            if (roboState != null) {
-                turnGunLeft(roboState.getNr());
-            } else {
-                RobocodeState state = robocodeStateHelper.create(this);
-                MotionAction action = strategy.getAction(state);
-                performAction(action);
-            }
+            RobocodeState state = robocodeStateHelper.create(this);
+            MotionAction action = strategy.getAction(state);
+            performAction(action);
         }
 
     }
@@ -72,24 +65,5 @@ public class LearningRobot extends AdvancedRobot {
         fire(1);
     }
 
-    @Override
-    public void onRoundEnded(RoundEndedEvent event) {
-        RoboState state = new RoboState();
-        state.setNr(getRoundNum() * 4);
-        helper.persist(state);
-    }
 
-    @Override
-    public void onBattleEnded(BattleEndedEvent event) {
-        RoboState state = new RoboState();
-        state.setNr(getRoundNum() * 4);
-        helper.persist(state);
-    }
-
-    @Override
-    public void onWin(WinEvent event) {
-        RoboState state = new RoboState();
-        state.setNr(getRoundNum() * 4);
-        helper.persist(roboState);
-    }
 }
